e73d30147fc402d945cbfc63b24de39618004b8f
Tim <1684200440@qq.com>
2018-08-12 14:26:47 +0800

    userapi
    用户功能介绍

--- writeme-service/src/main/java/com/zust/writeme/dao/UserMapper.java
+++ writeme-service/src/main/java/com/zust/writeme/dao/UserMapper.java
@@ -3,5 +3,13 @@ package com.zust.writeme.dao;
 import com.zust.writeme.config.MyMapper;
 import com.zust.writeme.model.User;
 
+import java.util.List;
+
 public interface UserMapper extends MyMapper<User> {
+    User selectByAccount(String account);
+    List<User> selectByUserName(String name);
+    boolean deleteUser(int uid);
+    void updatePassword(int uid ,String password);
+    void updateUserName(int uid, String name);
+
 }
