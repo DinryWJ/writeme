bc6a33dd0d9173311627fc2f92ae2c7c62fe5397
吴佳杰 <wujiajie@zhuojianchina.com>
2018-08-17 18:05:00 +0800

    upload

--- writeme-service/src/main/java/com/zust/writeme/api/UploadApi.java
+++ writeme-service/src/main/java/com/zust/writeme/api/UploadApi.java
@@ -1,5 +1,6 @@
 package com.zust.writeme.api;
 
+import com.zust.writeme.common.util.FtpFileUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -9,23 +10,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 @Api(value = "图片上传", description = "图片上传")
+@CrossOrigin
 @RequestMapping(value = "/upload")
-@Controller
+@RestController
 public class UploadApi {
     private static final Logger log = LoggerFactory.getLogger(UploadApi.class);
 
     @ApiOperation(value = "图片上传", notes = "图片上传")
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public ResponseEntity<ApiResponse> upload(){
+    public ResponseEntity<ApiResponse> upload(@RequestParam(value = "file") MultipartFile multipartFile) {
         JSONObject obj = new JSONObject();
-        obj.put("url","static/1.jpg");
+        obj.put("url", "http://www.dinry.top/a.jpg");
+        ApiResponse res = new ApiResponse(200, "ok", obj);
+        return ApiResponse.successResponse(res);
+    }
+
+    @ApiOperation(value = "图片上传2", notes = "图片上传2")
+    @RequestMapping(value = "/upload2", method = RequestMethod.POST)
+    public ResponseEntity<ApiResponse> upload2(@RequestParam("file") MultipartFile file) throws IOException {
+
+        String fileName = file.getOriginalFilename();
+        InputStream inputStream = file.getInputStream();
+        String filePath = null;
+
+
+        Boolean flag = FtpFileUtil.uploadFile(fileName, inputStream);
+        if (flag == true) {
+            System.out.println("ftp上传成功！");
+            filePath = fileName;
+        }
+        JSONObject obj = new JSONObject();
+        obj.put("url", "http://www.dinry.top/"+filePath );
         ApiResponse res = new ApiResponse(200, "ok", obj);
         return ApiResponse.successResponse(res);
     }
